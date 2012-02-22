c1c00fba0b456f0a6404f9ecdf213d1a18b4ecf2
Akihiro Komori <akihiro.komori@gmail.com>
2012-02-22 12:39:00 +0900

    added

---
+++ android/SpeechToSocket/src/com/blogspot/comolog/speechtosocket/SpeechToSocketActivity.java
@@ -0,0 +1,15 @@
+package com.blogspot.comolog.speechtosocket;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeechToSocketActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
