dbc8c9c7dfa60289fe388429401bd06d3dd80bb3
Akihiro Komori <akihiro.komori@gmail.com>
2012-03-21 00:07:43 +0900

    intermediage codes

--- html/css3-test/js/script.js
+++ html/css3-test/js/script.js
@@ -3,11 +3,23 @@
  */
 $(function() {	
 	$("#fadethrough .incoming").click(function () {
-		$(this).css("opacity", "1");
-		$(this).css("-webkit-animation-name", "fadethrough");
-		var el = $(this),
-		newone = el.clone(true);
-		el.before(newone);
-		$("." + el.attr("class") + ":last").remove();
+
+		$(this).toggleClass('active');
+				
+		if ($(this).hasClass('active')) {
+			$('#message').html('Click the image to reset');
+//			$(this).bind("webkitAnimationEnd",function(){
+//				console.log('webkitAnimationEnd');
+//			});			
+		} else {
+			$('#message').html('Click the image to start transition');			
+		}
+		
+//		$(this).css("opacity", "1");
+//		$(this).css("-webkit-animation-name", "fadethrough");
+//		var el = $(this),
+//		newone = el.clone(true);
+//		el.before(newone);
+//		$("." + el.attr("class") + ":last").remove();
 	});
 });
