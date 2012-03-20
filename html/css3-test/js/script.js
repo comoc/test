ae1de155e341e47a9cafebe1a1ff344400dc3bee
Akihiro Komori <akihiro.komori@gmail.com>
2012-03-20 18:41:19 +0900

    modified

--- html/css3-test/js/script.js
+++ html/css3-test/js/script.js
@@ -1,8 +1,13 @@
-/* Author:
-
-*/
-
-
-
-
-
+/* Author: Akihiro Komori
+ @see <a href="http://css-tricks.com/restart-css-animation/">Restart CSS Animation</a> 
+ */
+$(function() {	
+	$("#fadethrough .incoming").click(function () {
+		$(this).css("opacity", "1");
+		$(this).css("-webkit-animation-name", "fadethrough");
+		var el = $(this),
+		newone = el.clone(true);
+		el.before(newone);
+		$("." + el.attr("class") + ":last").remove();
+	});
+});
